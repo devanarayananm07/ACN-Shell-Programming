3f5363cf5e04bf7bb79f7b956c43183914a9c87c
DEVANARAYANAN M <154665307+devanarayananm07@users.noreply.github.com>
2024-04-11 15:27:18 +0530

    Update 27)Pascal Traingle.sh

--- 27)Pascal Traingle.sh
+++ 27)Pascal Traingle.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
-function binom {
-if [ $2 -eq 0 ] || [ $2 -eq $1 ]; then
-echo 1
-else
-echo $(( $(binom $(($1-1)) $(($2-1))) + $(binom $(($1-1)) $2) ))
-fi
-}
-echo "Enter the number of rows in Pascal's triangle: "
+echo "Enter number of rows : "
 read rows
-for (( i=0; i<$rows; i++ )); do
-for (( j=0; j<=$i; j++ )); do
-val=$(binom $i $j)
-echo -n "$val "
+coef=1
+for((i=0;i<rows;i++))
+do
+for((space=1;space<=rows-i; space++))
+do
+echo -n "  "
 done
-# Move to next row
-echo ""
+for((j=0;j<=i;j++))
+do
+if [ $j -eq 0 -o $i -eq 0 ]
+then
+coef=1;
+else
+coef=$((coef*(i-j+1)/j))
+fi
+echo -n $coef "   "
+done
+echo
 done
